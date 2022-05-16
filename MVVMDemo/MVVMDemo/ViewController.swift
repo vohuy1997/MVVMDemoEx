566c85b10d5b95c5ff06430b55e090210a3bdf62
vo huy <vovuhuyk21@gmail.com>
2022-05-16 16:18:35 +0700

    Main

---
+++ MVVMDemo/MVVMDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by Võ Huy on 16/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
