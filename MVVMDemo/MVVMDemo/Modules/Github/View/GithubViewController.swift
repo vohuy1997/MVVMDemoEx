b048d18f879767c9e9684114a8542ed479421ee7
vo huy <vovuhuyk21@gmail.com>
2022-05-21 15:11:53 +0700

    [#1][ADD] layout github

---
+++ MVVMDemo/MVVMDemo/Modules/Github/View/GithubViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GithubViewController.swift
+//  MVVMDemo
+//
+//  Created by Võ Huy on 18/05/2022.
+//
+
+import UIKit
+
+class GithubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
